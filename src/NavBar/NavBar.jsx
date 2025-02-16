4316779c550b097a12a491c9bc30cd377cb570b7
Paz Spera <mspera03@gmail.com>
2025-02-16 20:51:44 -0300

    Agregado el componente NavBar

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import AppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+
+export default function NavBar() {
+  return (
+    <AppBar>
+        <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+            >
+                <MenuIcon />
+            </IconButton>
+            <p>Navbar</p>
+        </Toolbar>
+    </AppBar>
+  );
+}
