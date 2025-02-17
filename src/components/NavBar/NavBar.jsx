507206e440318188b746f07a0c92338e4d7bb805
Paz Spera <mspera03@gmail.com>
2025-02-17 02:01:59 -0300

    Agregado responsive a NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,70 @@
+import  {useState} from "react";
 import './Navbar.css';
-import { AppBar, Box, Container, Link, Toolbar, Typography } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Container, IconButton, Link, List, ListItem, ListItemIcon, ListItemText, SwipeableDrawer , Toolbar, Typography, useMediaQuery } from "@mui/material";
 import CartWidget from '../CartWidget/CartWidget';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export default function NavBar() {
+  const isMobile = useMediaQuery((theme)=> theme.breakpoints.down('sm'));
+
+  const MobileNav = () => {
+    const [openMobileMenu, setOpenMobileMenu] = useState(false);
+
+    const openDrawer = () => setOpenMobileMenu(true);
+    const closeDrawer = () => setOpenMobileMenu(false);
+
+    return (
+      <>
+        <SwipeableDrawer 
+          open={openMobileMenu}
+          onClose={closeDrawer}
+          onOpen={openDrawer}
+        >
+          <Box onClick={closeDrawer} onKeyDown={closeDrawer}>
+            <Typography variant="subtitle1"component="p">
+              Dados & Dragones
+            </Typography>
+            <List>
+              <ListItemText primary="Estrategia"></ListItemText>
+              <ListItemText primary="Familiares"></ListItemText>
+              <ListItemText primary="Party"></ListItemText>
+            </List>
+          </Box>
+        </SwipeableDrawer>
+        <IconButton onClick={openDrawer}>
+          <MenuIcon/>
+        </IconButton>
+      </>
+    )
+  }
+
+  const DesktopNav = () => {
+    return (
+      <>
+        <Toolbar className="navbar-container">
+          <Typography variant="subtitle1"component="p">
+            Dados & Dragones
+          </Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center', }}>
+            <Link className="navbar-link">Estrategia</Link>
+            <Link className="navbar-link">Familiares</Link>
+            <Link className="navbar-link">Party</Link>
+            <CartWidget />
+          </Box>
+        </Toolbar>
+      </>
+    )
+  }
+
   return (
     <>
       <AppBar>
-          <Container>
-            <Toolbar className="navbar-container">
-                <Typography 
-                  variant="subtitle1" 
-                  component="p"
-                >
-                  Dados & Dragones
-                </Typography>
-                <Box sx={{ display: 'flex', alignItems: 'center', }}>
-                  <Link className="navbar-link">Estrategia</Link>
-                  <Link className="navbar-link">Familiares</Link>
-                  <Link className="navbar-link">Party</Link>
-                  <CartWidget/>
-                </Box>
-            </Toolbar> 
-          </Container>
+        <Container>
+          {isMobile ? <MobileNav/> : <DesktopNav/>}
+        </Container>
       </AppBar>
     </>
-    
+
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ThemeProvider theme={theme}>  
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </ThemeProvider>
 )
