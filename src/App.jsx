71b311dd51082eec01f2b0c94f7c125b46487b27
Paz Spera <mspera03@gmail.com>
2025-03-03 23:32:07 -0300

    Creados componentes ItemDetail y ItemDetailContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
-import EjemploApi from "./components/EjemploApi/EjemploApi";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+
 
 function App() {
 	const greeting = "Hola Mundo";
@@ -10,7 +11,7 @@ function App() {
 		<>
 			<NavBar />
 			<ItemListContainer greeting={greeting} />
-      <EjemploApi/>
+      <ItemDetailContainer/>
 		</>
 	);
 }
---
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,10 @@
+
+export default function ItemDetail() {
+
+
+  return (
+    <>
+      <p>Soy el item detail</p>
+    </>
+  )
+}
