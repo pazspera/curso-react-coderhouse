85617ca7289bb0ae0cd1a5afd7d909447c582d2f
Paz Spera <mspera03@gmail.com>
2025-03-23 23:23:07 -0300

    Creada la vista CartView y el componente Cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,10 @@
+
+export default function Cart() {
+  // componente funcional
+  
+  return (
+    <>
+      <h2>El cart</h2>
+    </>
+  )
+}
---
+++ src/components/CartView/CartView.jsx
@@ -0,0 +1,13 @@
+import Cart from "../Cart/Cart";
+
+
+export default function CartView() {
+  // componente de presentación
+
+  return (
+    <>
+      <h1>Hola Cart!</h1>
+      <Cart></Cart>
+    </>
+  )
+}
