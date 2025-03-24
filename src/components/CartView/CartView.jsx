c52809377e90866e0168c90d69ddc9637aef3016
Paz Spera <mspera03@gmail.com>
2025-03-24 15:56:44 -0300

    Correcciones a CartWidget

--- src/components/CartView/CartView.jsx
+++ src/components/CartView/CartView.jsx
@@ -1,30 +1,44 @@
 import Cart from "../Cart/Cart";
 import { Grid, Container, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material"; 
+import { CartContext } from "../../context/CartContext";
+import { useContext } from "react";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 
 export default function CartView() {
   // componente de presentación
+  const { itemsInCart } = useContext(CartContext);
 
   return (
     <>
-      {<Container>
-        <Grid container rowSpacing={2} sx={{marginTop: 4, padding: 3, border: '1px solid red'}}>
-          <TableContainer component={Paper}>
-            <Table sx={{minWidth: 650}}>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Producto</TableCell>
-                  <TableCell>Producto</TableCell>
-                  <TableCell>Producto</TableCell>
-                  <TableCell>Producto</TableCell>                  
-                </TableRow>
-              </TableHead>
-            </Table>
-          </TableContainer>
-          
-
-        </Grid>
-      </Container>}
+      { itemsInCart === 0 
+        ?
+          (
+            <>
+              <p>El carrito de compras está vacío</p>
+              <Button size="medium" variant="contained" component={Link} to="/">Volver al inicio</Button>
+            </>
+          )
+        : (
+            <Container>
+              <Grid container rowSpacing={2} sx={{marginTop: 4, padding: 3, border: '1px solid red'}}>
+                <TableContainer component={Paper}>
+                  <Table sx={{minWidth: 650}}>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Producto</TableCell>
+                        <TableCell>Producto</TableCell>
+                        <TableCell>Producto</TableCell>
+                        <TableCell>Producto</TableCell>                  
+                      </TableRow>
+                    </TableHead>
+                  </Table>
+                </TableContainer>
+              </Grid>
+            </Container>
+        )
+      }
       
     </>
   )
