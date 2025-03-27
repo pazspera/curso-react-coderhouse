5520eb2c1262f733115aa5ea118bc703c9752732
Paz Spera <mspera03@gmail.com>
2025-03-26 23:37:11 -0300

    Agregado favicon y logoIcon a navbar

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,13 @@ export default function Logo() {
 
   return (
     <>
-      <Box sx={{ display: "flex", flexDirection: "row" }}>
+      <Box sx={{ display: "flex", flexDirection: "row", alignItems:"center" }}>
+        <CardMedia 
+          component="img"
+          width="48px"
+          image="/favicon.png"
+          className={styles.logoIcon}
+        />
         <Box className={styles.logoContainer}>
           <span className={styles.logoText}>Caladan Games</span>
         </Box>
