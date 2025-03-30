915cf5e9ee124a600944e613cbd30e47287e2e72
Paz Spera <mspera03@gmail.com>
2025-03-30 14:36:21 -0300

    Agregado Skeleton a CartProduct para que se muestre mientras carga la img en el Cart

--- src/components/CartProduct/CartProduct.jsx
+++ src/components/CartProduct/CartProduct.jsx
@@ -1,8 +1,8 @@
 import styles from "./CartProduct.module.css";
-import { Grid, Typography, CardMedia, Box } from "@mui/material";
+import { Grid, Typography, CardMedia, Box, Skeleton } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 import { CartContext } from "../../context/CartContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export default function CartProduct({product}) {
   // nombre producto
@@ -10,16 +10,23 @@ export default function CartProduct({product}) {
   // total producto = amount.precio (formato precio)
   // btn eliminar items (trash can)
   const { deleteItem } = useContext(CartContext);
+  const [loaded, setLoaded] = useState(false);
 
   return (
     <>
       <Grid item xs={12}>
         <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+          {/* skeleton de card media */
+            !loaded && (
+              <Skeleton variant="rectangular" width={90} height={90}></Skeleton>
+            )
+          }
           <CardMedia 
             component="img" 
             image={product.pictureUrlCart}
             alt={product.title}
             sx={{ width: 90, height: 90 }}
+            onLoad={()=> setLoaded(true)}
           />
           <Box className={styles.productContainer}>
             <Typography variant="body1">{product.title}</Typography>
