214e6a1b74eb3d3cb72f32596904e594e1694abd
Paz Spera <mspera03@gmail.com>
2025-04-06 16:03:04 -0300

    Movidos estilos a module para OrderDetail

--- src/components/OrderDetail/OrderDetail.jsx
+++ src/components/OrderDetail/OrderDetail.jsx
@@ -1,6 +1,7 @@
 import { Typography, Grid, Paper, Button } from "@mui/material"; 
 import { formatPrice } from "../../utils/utils";
 import { Link } from "react-router-dom";
+import styles from "./OrderDetail.module.css";
 
 export default function OrderDetail({ order }) {
   if(!order) {
@@ -8,33 +9,33 @@ export default function OrderDetail({ order }) {
   }
   return (
     <>
-      <Paper elevation={3} sx={{ margin: 2, padding: 3 }}>
+      <Paper elevation={3} className={styles.mainContainer} >
         <Grid container rowSpacing={2}>
           <Grid item xs={12} >
             <Typography variant="h3" gutterBottom>¡Gracias por tu compra!</Typography>
           </Grid>
           
-          <Grid item xs={12} md={6} sx={{ marginTop: 1, marginBottom: 1 }}>
+          <Grid item xs={12} md={6} className={styles.infoContainer}>
             <Typography variant="h6">Datos del comprador: </Typography>
             <Typography variant="body1">Nombre: {order.buyer.name}</Typography>
             <Typography variant="body1">Email: {order.buyer.email}</Typography>
             <Typography variant="body1">Teléfono: {order.buyer.phone}</Typography>
           </Grid>
           
-          <Grid item xs={12} md={6} sx={{ marginTop: 1, marginBottom: 1 }}>
+          <Grid item xs={12} md={6} className={styles.infoContainer}>
             <Typography variant="h6">Detalle de la compra: </Typography>
             {/* es necesario formatear la fecha o rompe la vista */}
             <Typography variant="body1">Fecha: {order.date ? order.date.toDate().toLocaleDateString() : undefined}</Typography>
             <Typography variant="body1">ID de la compra: {order.id}</Typography>
 
-            <Typography variant="subtitle1" sx={{ marginTop: 1, marginBottom: 1 }}>Productos: </Typography>
+            <Typography variant="subtitle1" className={styles.subtitle}>Productos: </Typography>
             {order.items.map((product)=> (
               <Typography variant="body1" key={product.id}>
                 - {product.title} x {product.quantity}: {formatPrice(product.quantity * product.price)} 
               </Typography>
             ))}
 
-            <Typography variant="subtitle1" sx={{ marginTop: 1, marginBottom: 1}}>Total: {formatPrice(order.total)}</Typography>
+            <Typography variant="subtitle1" className={styles.subtitle}>Total: {formatPrice(order.total)}</Typography>
           </Grid>
 
           <Grid item xs={12} >
